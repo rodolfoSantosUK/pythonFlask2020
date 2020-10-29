346ebde3be6cbf15c91c6f0e6f38a0207113df0b
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-10-29 20:43:46 -0300

    Tratando a deleção de hotel no banco de dados.

--- modelos/hotel.py
+++ modelos/hotel.py
@@ -42,3 +42,6 @@ class HotelModel(banco.Model):
         self.diaria = diaria
         self.cidade = cidade
 
+    def delete_hotel(self):
+        banco.session.delete(self)
+        banco.session.commit()
